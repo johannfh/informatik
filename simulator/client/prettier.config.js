e9ecd2288189b83affef55352324c86ef1bcdfe4
Johann Heuer <johann.f.heuer@gmail.com>
2025-01-12 13:55:07 +0100

    add svelte client

---
+++ simulator/client/prettier.config.js
@@ -0,0 +1,19 @@
+/** @type {import('prettier').Config} */
+const config = {
+    useTabs: false,
+    tabWidth: 4,
+    singleQuote: false,
+    trailingComma: "all",
+    printWidth: 100,
+    plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
+    overrides: [
+        {
+            files: "*.svelte",
+            options: {
+                parser: "svelte",
+            },
+        },
+    ],
+};
+
+export default config;
