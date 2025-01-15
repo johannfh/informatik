dc831ae6e2b878907927904278e77768bded6c86
Johann Heuer <johann.f.heuer@gmail.com>
2025-01-15 22:13:50 +0100

    rewrite

--- simulator/client/src/routes/+layout.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-    import "../app.css";
-    let { children } = $props();
-</script>
-
-<article class="bg-gray-50 dark:bg-zinc-900 text-black dark:text-white w-[100dvw] h-[100dvh] py-4 px-8">
-    {@render children()}
-</article>
